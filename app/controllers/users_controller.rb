168a911ae384d2a7d37b51c872090f8f2c52c7cf
Risa Aki <risa-aki@united.jp>
2016-04-17 14:24:30 +0000

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -16,11 +17,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  
+ def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :area, :password,
                                  :password_confirmation)
+end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+    
   end
 end
