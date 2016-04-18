01ec40edb67a2a9eae9b38d7a8c3e509982126fb
Risa Aki <risa-aki@united.jp>
2016-04-18 11:48:37 +0000

    last mission

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416063027) do
+ActiveRecord::Schema.define(version: 20160418011238) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
