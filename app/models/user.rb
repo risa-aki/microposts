a4e15ba814be125aa1c70e9795ac661b0dd919c1
Risa Aki <risa-aki@united.jp>
2016-04-18 11:14:31 +0000

    twitter follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,21 @@
 class User < ActiveRecord::Base
-  
-  before_save { self.email = self.email.downcase }
+ before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false } 
   has_secure_password
   has_many :microposts
-
   has_many :following_relationships, class_name:  "Relationship",
                                      foreign_key: "follower_id",
                                      dependent:   :destroy
-                                     
-                                     
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
-  has_many :following, through: :active_relationships,  source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-                                     
-                                     
-                                     
-                                     
   has_many :following_users, through: :following_relationships, source: :followed
-  
-   has_many :follower_relationships, class_name:  "Relationship",
+  has_many :follower_relationships, class_name:  "Relationship",
                                     foreign_key: "followed_id",
                                     dependent:   :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
-  
-  
-  def feed_items
-    Micropost.where(user_id: following_user_ids + [self.id])
-  end
+
   
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -49,22 +31,10 @@ class User < ActiveRecord::Base
   # あるユーザーをフォローしているかどうか？
   def following?(other_user)
     following_users.include?(other_user)
-  end 
-    
-    
-    
-    # ユーザーをフォローする
-  def follow(other_user)
-    active_relationships.create(followed_id: other_user.id)
   end
-
-  # ユーザーをアンフォローする
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
+  
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
   end
-
-  # 現在のユーザーがフォローしてたらtrueを返す
-  def following?(other_user)
-    following.include?(other_user)
-  end
+  
 end
