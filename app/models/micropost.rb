10c9dfbaae732ab0f7d3a64ad08185dd498542d1
Risa Aki <risa-aki@united.jp>
2016-04-18 01:11:20 +0000

    add posting microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,9 @@
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+end
+
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+end
